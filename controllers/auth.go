2cf995d4e295056c8bab56b3b95e95662a31d400
Arnav Sankaran <arnavsankaran@gmail.com>
2020-03-03 18:48:26 -0600

    Add login controller

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,11 +1,50 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/acm-uiuc/core/controllers/context"
 )
 
-func LoginController(ctx *context.CoreContext) error {
-	return ctx.String(http.StatusOK, "Hello World!")
+type LoginRequest struct {
+	Username string `json:"username`
+	Password string `json:"password`
+}
+
+type LoginResponse struct {
+	Success bool
+	Token string
+	Message string
+}
+
+func (controller *Controller) LoginController(ctx *context.CoreContext) error {
+	req := &LoginRequest {}
+	err := ctx.Bind(req)
+	if err != nil {
+		return fmt.Errorf("failed to bind: %w",
+			ctx.JSON(http.StatusInternalServerError, &LoginResponse {
+				Success: false,
+				Token: "",
+				Message: "Internal Error",
+			}),
+		)
+	}
+
+	token, err := controller.svcs.Auth.Login(req.Username, req.Password)
+	if err != nil {
+		return fmt.Errorf("failed to login: %w",
+			ctx.JSON(http.StatusUnauthorized, &LoginResponse {
+				Success: false,
+				Token: "",
+				Message: "Invalid Login Details",
+			}),
+		)
+	}
+
+	return ctx.JSON(http.StatusOK, &LoginResponse {
+		Success: true,
+		Token: token,
+		Message: "Successful Login",
+	})
 }
--- controllers/controllers.go
+++ controllers/controllers.go
@@ -11,16 +11,23 @@ import (
 	"github.com/acm-uiuc/core/controllers/context"
 )
 
-type Controller struct {}
+type Controller struct {
+	*echo.Echo
+	svcs services.Services
+}
 
-func New(svcs services.Services) *echo.Echo {
-	controller := echo.New()
+func New(svcs services.Services) Controller {
+	controller := Controller {
+		echo.New(),
+		svcs,
+	}
+
 	controller.Use(middleware.Logger())
 	controller.Use(middleware.Recover())
 	controller.Use(middlewares.ContextExtender)
 	// TODO: Add more middlewares
 
-	controller.POST("/auth/login", ContextConverter(LoginController))
+	controller.POST("/auth/login", ContextConverter(controller.LoginController))
 	// TODO: Add more routes
 
 	return controller
