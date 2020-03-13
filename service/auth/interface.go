a58862404a34e2eea199a0a7de7db6866f56eb70
Arnav Sankaran <arnavsankaran@gmail.com>
2020-03-13 11:39:12 -0500

    Add verify operation to auth

--- service/auth/interface.go
+++ service/auth/interface.go
@@ -7,6 +7,7 @@ import (
 type AuthService interface {
 	GetOAuthRedirect(provider string) (string, error)
 	Authorize(provider string, code string) (*model.Token, error)
+	Verify(token string) (bool, string, error)
 }
 
 func New() (AuthService, error) {
