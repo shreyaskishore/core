7cb08a241c1f6636c8c3f518ac7f3470bd24df5c
Arnav Sankaran <arnavsankaran@gmail.com>
2020-03-14 15:05:00 -0500

    Add ability to run migrations

--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
-	_ "github.com/acm-uiuc/core/database/migration"
+	"github.com/acm-uiuc/core/database/migration"
 	"github.com/acm-uiuc/core/server"
 )
 
+type options struct {
+	migration string
+	server    bool
+}
+
 func main() {
-	svr, err := server.New()
-	if err != nil {
-		fmt.Println(err)
+	opts := options{}
+	flag.StringVar(&opts.migration, "migration", "", "migration to start from")
+	flag.BoolVar(&opts.server, "server", false, "enable to run the server")
+	flag.Parse()
+
+	if opts.migration != "" {
+		err := migration.Migrate(opts.migration)
+		if err != nil {
+			fmt.Printf("%s\n", err.Error())
+			return
+		}
+
+		return
 	}
 
-	svr.Start(":8000")
+	if opts.server {
+		svr, err := server.New()
+		if err != nil {
+			fmt.Printf("%s\n", err.Error())
+			return
+		}
+
+		err = svr.Start(":8000")
+		if err != nil {
+			fmt.Printf("%s\n", err.Error())
+			return
+		}
+
+		return
+	}
 }
