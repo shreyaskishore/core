adab4dd7d7f88d06bc49cf70326735f1dbb41170
Arnav Sankaran <arnavsankaran@gmail.com>
2020-03-15 00:24:41 -0500

    Add old rp sites

--- controller/site/impl.go
+++ controller/site/impl.go
@@ -74,8 +74,108 @@ func (controller *SiteController) ReflectionsProjections(ctx *context.Context) e
 			Uri  string
 		}{
 			{
+				Year: 2020,
+				Uri:  "https://2020.reflectionsprojections.org",
+			},
+			{
 				Year: 2019,
 				Uri:  "https://2019.reflectionsprojections.org",
+			},
+			{
+				Year: 2018,
+				Uri:  "https://reflectionsprojections.github.io/rp2018",
+			},
+			{
+				Year: 2017,
+				Uri:  "https://reflectionsprojections.github.io/rp2017",
+			},
+			{
+				Year: 2016,
+				Uri:  "https://reflectionsprojections.github.io/rp2016",
+			},
+			{
+				Year: 2015,
+				Uri:  "https://reflectionsprojections.github.io/rp2015",
+			},
+			{
+				Year: 2014,
+				Uri:  "https://reflectionsprojections.github.io/rp2014",
+			},
+			{
+				Year: 2013,
+				Uri:  "https://reflectionsprojections.github.io/rp2013",
+			},
+			{
+				Year: 2012,
+				Uri:  "https://reflectionsprojections.github.io/rp2012",
+			},
+			{
+				Year: 2011,
+				Uri:  "https://reflectionsprojections.github.io/rp2011",
+			},
+			{
+				Year: 2010,
+				Uri:  "https://reflectionsprojections.github.io/rp2010",
+			},
+			{
+				Year: 2009,
+				Uri:  "https://reflectionsprojections.github.io/rp2009",
+			},
+			{
+				Year: 2008,
+				Uri:  "https://reflectionsprojections.github.io/rp2008",
+			},
+			{
+				Year: 2007,
+				Uri:  "https://reflectionsprojections.github.io/rp2007",
+			},
+			{
+				Year: 2006,
+				Uri:  "https://reflectionsprojections.github.io/rp2006",
+			},
+			{
+				Year: 2005,
+				Uri:  "https://reflectionsprojections.github.io/rp2005",
+			},
+			{
+				Year: 2004,
+				Uri:  "https://reflectionsprojections.github.io/rp2004",
+			},
+			{
+				Year: 2003,
+				Uri:  "https://reflectionsprojections.github.io/rp2003",
+			},
+			{
+				Year: 2002,
+				Uri:  "https://reflectionsprojections.github.io/rp2002",
+			},
+			{
+				Year: 2001,
+				Uri:  "https://reflectionsprojections.github.io/rp2001",
+			},
+			{
+				Year: 2000,
+				Uri:  "https://reflectionsprojections.github.io/rp2000",
+			},
+			{
+				Year: 1999,
+				Uri:  "https://reflectionsprojections.github.io/rp1999",
+			},
+			{
+				Year: 1998,
+				Uri:  "https://reflectionsprojections.github.io/rp1998",
+			},
+			{
+				Year: 1997,
+				Uri:  "https://reflectionsprojections.github.io/rp1997",
+			},
+			{
+				Year: 1996,
+				Uri:  "https://reflectionsprojections.github.io/rp1996",
+			},
+			{
+				Year: 1995,
+				Uri:  "https://reflectionsprojections.github.io/rp1995",
 			},
 		},
 	}
